32e0ecd0cf5bec74232437ad6614f6a147a1ca2f
Gergo Gyori <gergo.gyori@gmail.com>
2024-03-24 21:00:07 +0100

    added bikelane vis

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,8 @@ def main():
     st.title("Test page")
     st.write("This is a demo of Streamlit")
 
-    st.session_state["number_of_elements"] = st.slider("Number of elements in the forest dataset", 0, 100000, 1)
+    st.session_state["number_of_elements"] = st.slider("Number of elements in the forest and bikelane dataset", 0, 100000, 1)
+    st.warning("Data is schuffled before visualization.")
     st.write(st.session_state["number_of_elements"])
 
     if st.button("Show a test map"):
@@ -27,8 +28,18 @@ def main():
         st_map2 = folium_static(initFoliumMap(gdf))
         
         st.write("**Forest areas**")
+        st.write("Data is shuffled before visualization")
         # Example usage
         gdf_2 = gpd.read_parquet("dataset/raw_unprocessed/forest_simplified.parquet")  # Load your GeoDataFrame
+        gdf_2 = gdf_2.sample(frac=1).reset_index(drop=True)
         st_map2 = folium_static(initFoliumMap(gdf_2, number_of_elements=st.session_state["number_of_elements"]))
+
+        st.write("**Bike lanes**")
+        st.write("Data is shuffled before visualization")
+        st.error("Query parameters are obviusly wrong, but this is just a demo")
+        gdf_3 = gpd.read_parquet("dataset/raw_unprocessed/bikelane.parquet")  # Load your GeoDataFrame
+        # shuffle the data
+        gdf_3 = gdf_3.sample(frac=1).reset_index(drop=True)
+        st_map3 = folium_static(initFoliumMap(gdf_3, number_of_elements=st.session_state["number_of_elements"]))
 
 main()
