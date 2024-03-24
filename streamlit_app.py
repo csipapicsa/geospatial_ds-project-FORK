5947ef8254d22d1b09ce4d0e356e2cb09f5a48f7
Gergo Gyori <gergo.gyori@gmail.com>
2024-03-24 15:47:10 +0100

    maps, updated site, etc

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,34 @@
 import streamlit as st
 import functions as f
-from streamlit_functions import geodataframe_to_map_converter
+from streamlit_functions import geodataframe_to_map_converter, visualize_geodata, initFoliumMap
+import geopandas as gpd
+from streamlit_folium import folium_static
 
 def main():
     st.title("Test page")
     st.write("This is a demo of Streamlit")
 
+    st.session_state["number_of_elements"] = st.slider("Number of elements in the forest dataset", 0, 10000, 1)
+    st.write(st.session_state["number_of_elements"])
+
     if st.button("Show a test map"):
+        """
         gdf = f.open_layer_from_db(database_name='test_database.db',
                      table_name='test',
                      crs = f.CRS)
 
         st.map(geodataframe_to_map_converter(gdf))
+        """
+
+        st.write("**Groceries, supermarkets*")
+
+        gdf = gpd.read_parquet("dataset/raw_unprocessed/grocery.parquet")  # Load your GeoDataFrame
+
+        st_map2 = folium_static(initFoliumMap(gdf))
+        
+        st.write("**Forest areas**")
+        # Example usage
+        gdf_2 = gpd.read_parquet("dataset/raw_unprocessed/forest_simplified.parquet")  # Load your GeoDataFrame
+        st_map2 = folium_static(initFoliumMap(gdf_2, number_of_elements=st.session_state["number_of_elements"]))
 
 main()
--- streamlit_functions.py
+++ streamlit_functions.py
@@ -1,3 +1,7 @@
+import folium
+from streamlit_folium import folium_static
+import streamlit as st
+
 def geodataframe_to_map_converter(gdf):
     """
     Display a geodataframe on a map.
@@ -13,3 +17,45 @@ def geodataframe_to_map_converter(gdf):
     gdf['longitude'] = longitude
 
     return gdf
+
+#---------------------------------------------------------------------------------------------#
+#                                                                    VISUALIZATION            #
+#---------------------------------------------------------------------------------------------#
+
+def visualize_geodata(gdf):
+    # this is slow like a shit if the dataset is big 
+    # Center the map on the geometric center of the GeoDataFrame
+    center = gdf.geometry.centroid.unary_union.centroid.coords[:][0]
+    
+    # Create a Folium map object
+    m = folium.Map(location=center, zoom_start=12)
+
+    # Add geometries from the GeoDataFrame to the map
+    for _, row in gdf.iterrows():
+        # Simplify geometry for faster rendering
+        sim_geo = row.geometry.simplify(tolerance=0.001, preserve_topology=True)
+        geo_j = folium.GeoJson(data=sim_geo.__geo_interface__,
+                               style_function=lambda x: {'fillColor': 'green', 'color': 'green'})
+        geo_j.add_to(m)
+
+    # Display the map in Streamlit
+    folium_static(m)
+
+
+def initFoliumMap(gdf, number_of_elements=[]):
+    if number_of_elements != []:
+        if number_of_elements > len(gdf):
+            st.error(f"Requested number of elements ({number_of_elements}) exceeds the total available in the GeoDataFrame ({len(gdf)}). Please specify a smaller number.")
+            raise ValueError(f"Requested number of elements ({number_of_elements}) exceeds the total available in the GeoDataFrame ({len(gdf)}). Please specify a smaller number.")
+    layers = {}
+    # ma = folium.Map([47, 19.7], zoom_start=7, width='100%')
+    ma = folium.Map()
+    folium.TileLayer("openstreetmap").add_to(ma)
+    layers["Final Result"] = folium.FeatureGroup(name="Final Result")
+    if number_of_elements == []:
+        ma.add_child(layers["Final Result"].add_child(folium.GeoJson(data=gdf,style_function=lambda x:{'fillColor': "red", 'fillOpacity': 0.5, "opacity": 1, "weight": 2})))
+    else:
+        ma.add_child(layers["Final Result"].add_child(folium.GeoJson(data=gdf.head(number_of_elements),style_function=lambda x:{'fillColor': "red", 'fillOpacity': 0.5, "opacity": 1, "weight": 2})))
+    ma.fit_bounds(ma.get_bounds())
+
+    return ma
